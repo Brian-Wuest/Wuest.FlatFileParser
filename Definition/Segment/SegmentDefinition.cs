3970968615ff8789be67e7288e004f90e5e78d61
Brian Wuest <brian.wuest@outlook.com>
2022-12-13 07:03:50 -0600

    Adding base segment files

---
+++ Definition/Segment/SegmentDefinition.cs
@@ -0,0 +1,24 @@
+﻿namespace Wuest.FlatFileParser.Definition.Segment
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Wuest.FlatFileParser.Definition.Enums;
+
+	public abstract class SegmentDefinition
+	{
+		#region Properties
+
+		public string Name { get; set; }
+
+		public Object OrientationSettings { get; set; }
+
+		public Object ExtractionSettings { get; set; }
+
+		public SegmentType SegmentType { get; set; }
+
+		#endregion
+	}
+}
---
+++ FlatFile/Segment/ISegmentValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wuest.FlatFileParser.FlatFile.Segment
+{
+	public interface ISegmentValue
+	{
+		#region Properties
+
+
+
+		#endregion
+
+		#region Methods
+
+		void SetValue(Object value);
+
+		string GetFormattedValue();
+
+		#endregion
+	}
+}
